2660ae87cf48a87198cc6c15aa758f518e911404
Jugaire Limcatchon <101692991+JLimCatchon@users.noreply.github.com>
2023-03-20 07:58:32 +0800

    exam

---
+++ scripts/main.js
@@ -0,0 +1,207 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var player;
+var stars;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+
+var scoreText;
+var starCollected = 0;
+var starScoreText;
+var colors = [0xff0000, 0xffa500, 0xffff00, 0x008000, 0x0000ff, 0x4b0082, 0xee82ee];
+var currentColorIndex = 0;
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+}
+
+function create ()
+{
+    //  A simple background for our game
+    this.add.image(400, 300, 'sky');
+
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = this.physics.add.staticGroup();
+
+    //  Here we create the ground.
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    //  Now let's create some ledges
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    // The player and its settings
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    //  Player physics properties. Give the little guy a slight bounce.
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    //  Our player animations, turning, walking left and walking right.
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    //  Input Events
+    cursors = this.input.keyboard.createCursorKeys();
+
+    //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 0,
+        setXY: { x: Math.random() * game.config.width - 7, y: Math.random() * game.config.height - 70, stepX: 70 }
+    });
+    stars.children.iterate(function (child) {
+    
+        //  Give each star a slightly different bounce
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+
+    });
+
+    bombs = this.physics.add.group();
+
+    //  The score
+
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+    starScoreText = this.add.text(16, 40, 'Stars Collected: 0', { fontSize: '32px', fill: '#000' });
+
+    // Lost Text Box
+    this.lostTextBox = this.add.graphics();
+    this.lostTextBox.fillStyle(0xffffff, 0.8);
+    this.lostTextBox.fillRect(200, 200, 400, 200);
+    this.lostTextBox.visible = false;
+    this.lostTextBox = this.add.text(400, 250, 'You lost\nScore: 0', {
+        fontSize: '32px',
+        fill: '#000',
+        align: 'center'
+        });
+    this.lostTextBox.setOrigin(0.5);
+    this.lostTextBox.visible = false;      
+    
+
+    //  Collide the player and the stars with the platforms
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(bombs, platforms);
+
+    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
+    this.physics.add.overlap(player, stars, collectStar, playerColors, null, this);
+
+    this.physics.add.collider(player, bombs, hitBomb, null, this);
+}
+
+function update ()
+{
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
+}
+
+function collectStar (player, star)
+{
+    star.disableBody(true, true);
+
+    //  Add and update the score
+    score += 10;
+    starCollected += 1;
+    scoreText.setText('Score: ' + score);
+    starScoreText.setText('Stars Collected: ' + starCollected);
+
+    if (starCollected % 5 === 0) {
+        // Increase the player's size/scale by 10%
+        player.setScale(player.scaleX + 0.1, player.scaleY + 0.1);
+    }
+
+    if (stars.countActive(true) === 0)
+    {
+
+        //  A new batch of stars to collect
+        stars.children.iterate(function (child) {
+            child.enableBody(true, Math.random() * game.config.width, 0, true, true);
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
+    }
+}
+
+
+function hitBomb (player, bomb)
+{
+    this.physics.pause();
+
+    player.disableBody(true,true);
+    this.lostTextBox.setText('Game Over!\nScore: ' + score + '\nStar Collected: '+ starCollected);
+    this.lostTextBox.visible = true;
+    }
+function playerColors(){
+    
+      player.setTint(colors[currentColorIndex]);
+      currentColorIndex = (currentColorIndex + 1) % colors.length;
+}
